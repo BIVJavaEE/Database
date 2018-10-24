155a649461cb6de335486c15e34acd04a525ec1d
Valentin Bourcier <bourcier.valentin.a@gmail.com>
2018-10-24 17:47:43 +0200

    External configuration

--- src/main/java/mqtt/Subscriber.java
+++ src/main/java/mqtt/Subscriber.java
@@ -1,12 +1,13 @@
 package mqtt;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import javax.persistence.PersistenceException;
 import javax.persistence.TransactionRequiredException;
 
 import org.eclipse.paho.client.mqttv3.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import entity.Measure;
 import mapping.Mapper;
@@ -15,14 +16,7 @@ import mapping.MapperFactoryException;
 
 public class Subscriber implements MqttCallback {
 	
-	// MQTT client id
-	private String id;
-	
-	// MQTT server URI
-	private String uri;
-	
-	// Quality of service
-	private int qos;
+	private JsonNode configuration;
 	
 	// JSON parser to unserialize objects
 	private Parser<Measure> parser;
@@ -32,40 +26,36 @@ public class Subscriber implements MqttCallback {
 	
 	private IMqttClient client;
 	
-	public Subscriber(String uri, int qos) {
-		this.id = UUID.randomUUID().toString();
-		this.uri = uri;
-		this.qos = qos;
+
+	public Subscriber(JsonNode configuration) {
+		this.configuration = configuration;
 		this.parser = null;
 		this.mapper = null;
 	}
 	
 	protected void initialize() throws MapperFactoryException {
 		MapperFactory<Measure> mapperFactory = new MapperFactory<>();
-		this.mapper = mapperFactory.createMapper("~/DATABASE");
+		this.mapper = mapperFactory.createMapper(configuration.get("name").asText());
 		this.parser = new Parser<>(Measure.class);
 	}
 	
-	public void connect(MqttConnectOptions options, String topic) throws MqttException, MapperFactoryException {
-		initialize();
-		this.client = new MqttClient(this.uri, this.id);
+	public void connect(MqttConnectOptions options, String uri, String topic, String uuid, int qos) throws MqttException, MapperFactoryException {
+		this.client = new MqttClient(uri, uuid);
 		this.client.setCallback(this);
 		this.client.connect(options);	    
-        this.client.subscribe(topic, this.qos);
+        this.client.subscribe(topic, qos);
 	}
 	
     public void connectionLost(Throwable cause) {
+    	cause.printStackTrace();
         System.out.println("Connection lost because of an unhandled error: " + cause);
     }
 
     public void messageArrived(String topic, MqttMessage message) throws MqttException {
 	    try {
-	    	Measure measure = this.parser.parse(message.toString());
-	    	this.mapper.save(measure);
+	    	// TODO : Parse message and record data in DB
 	    	System.out.println("Data saved: " + message.toString());
-	    }catch (IOException e) {
-			System.out.println("Parsing data failed : " + e.getMessage());
-		}catch (IllegalArgumentException | TransactionRequiredException e) {
+	    }catch (IllegalArgumentException | TransactionRequiredException e) {
 			System.out.println("Ignoring last sensor data: " + message.toString());
 		}catch (PersistenceException e) {
 			System.out.println("Ignoring data that not respect model and database constraints. Reasons: " + e.getMessage());
